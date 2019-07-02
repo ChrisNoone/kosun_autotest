ea35f607d482edb862001112e4eb669b13519f5c
kosun.super <kosun.super@gamil.com>
2019-07-02 14:34:28 +0800

    fusion

--- Common/box.py
+++ Common/box.py
@@ -13,8 +13,6 @@ from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.common.keys import Keys
-import traceback
 
 
 class BaseDriver(object):
